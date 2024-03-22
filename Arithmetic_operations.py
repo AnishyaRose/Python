a6e16144732c73907f1dfe5c83f2dbaed55f61c8
AnishyaRose <anishya.rose@gmail.com>
2024-03-22 17:47:12 +0530

    python programs created

---
+++ Arithmetic_operations.py
@@ -0,0 +1,9 @@
+a =int(input())
+b=int(input())
+c=int(input())
+sum =( a + b + c)
+mul =(a * b * c)
+res = mul / sum
+print("The sum is:",sum)
+print("The mul is:",mul)
+print("The res is:",res)
---
+++ variable_name_age.py
@@ -0,0 +1,4 @@
+name= input("Enter your name")
+age = input(" Enter your Age")
+print("My name is :",name)
+print(" My Age is :",age)
